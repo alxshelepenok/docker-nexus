0d8d650f133e4135773c3a1cc2cecf6c7e114edd
Alexander Shelepenok <alxshelepenok@gmail.com>
2024-06-24 07:28:14 +0000

    chore: update nexus image

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -ex
 IMAGE_NAME="alxshelepenok/nexus"
 REGISTRY="docker-hosted.nexus.infrastructure.alxshelepenok.com"
 TAG="${1}"
-docker build -t ${IMAGE_NAME}:"${TAG}" .
+docker build --no-cache -t ${IMAGE_NAME}:"${TAG}" .
 docker tag ${IMAGE_NAME}:"${TAG}" ${REGISTRY}/${IMAGE_NAME}:"${TAG}"
 docker push ${REGISTRY}/${IMAGE_NAME}:"${TAG}"
